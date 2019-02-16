5df1ee4116551a49acefd375816b9b6cb4e4bc92
Adam La Morre <adam.lamorre@mail.mcgill.ca>
2019-02-16 12:59:44 -0500

    ready for otherpoint

--- src/ca/mcgill/ecse409/a2/q2.java
+++ src/ca/mcgill/ecse409/a2/q2.java
@@ -1,152 +1,165 @@
 package ca.mcgill.ecse409.a2;
 
+import java.util.Random;
 import java.util.ArrayList;
 
 public class q2 {
-    public static void main(String[] args){
-        Node n1 = new Node(1, 1.0, 1.0); // (1.0, 1.0)
-        Node n2 = new Node(2, 0.0, 1.0); // (0.0, 1.0)
-        Node n3 = new Node(3, 0.9, 0.5); // (0.9, 0.5)
-        Node n4 = new Node(4, 0.0, 0.0); // (0.0, 0.0)
-        Node n5 = new Node(5, 0.5, 0.0); // (0.5, 0.0)
 
-        Node[] nodes = {n1, n2, n3, n4, n5};
-        Edge[] edges = basicTriangulation(nodes);
-        Edge[] hull = getHull(edges);
+    public static int n,t; // constants
+    public static Point[] points;
+    public static ArrayList<Edge> edges = new ArrayList<Edge>();
 
-        for(Edge e : edges){
-            System.out.println( e.n1.id+ ", " + e.n2.id);
-        }
-    }
-
-    private static class Node {
-        private int id;
-        private double x;
-        private double y;
-
-        public Node(int id, double x, double y){
-            this.id = id;
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-    private static class Edge {
-        private Node n1;
-        private Node n2;
-
-        public Edge(Node n1, Node n2){
-            // Add some code to set lower id as n1
-            if(n1.id <= n2.id){
-                this.n1 = n1;
-                this.n2 = n2;
-            }else{
-                this.n1 = n2;
-                this.n2 = n1;
+    // Returns true if any existing edge intersects this one
+    public static boolean intersection(Edge f) {
+        for (Edge e : edges) {
+            if (f.intersects(e)) {
+                return true;
             }
         }
+        return false;
     }
 
-    public static double getSlope(Edge edge){
-        return (edge.n1.y - edge.n2.y)/(edge.n1.x - edge.n2.x);
-    }
-
-    public static double getLength(Edge edge){
-        double dx = edge.n1.x = edge.n2.x;
-        double dy = edge.n1.y = edge.n2.y;
-        return Math.sqrt((dx*dx) + (dy*dy));
-    }
-
-    /**
-     * This method tell is a node is above or below the edges line
-     * True is above, False is equal or below.
-     * */
-    public static boolean isAbove(Edge edge, Node node){
-        // Create a line function
-        double slope = getSlope(edge);
-        double yInt = edge.n1.y - (slope * edge.n1.x);
-        // Is node's x, y is above that function True, else False
-        if(node.y >= (yInt + slope * node.x)){
-            return true;
-        }else{
-            return false;
+    public static Point[] shareTwoPoints(Edge edge){
+        Point p = edge.p;
+        Point q = edge.q;
+        Object[] pEdges = p.edges.toArray();
+        for(Object e : p.edges.toArray()){
+            System.out.println(e.toString());
         }
+        return null;
     }
 
-    /**
-     * This method will check if edge1 and edge2 are crossing one another
-     * If one node is above edge1 and one node is below edge1 then they're crossing
-     * True if the cross, False o.t.w.
-     * */
-    public static boolean isCrossing(Edge edge1, Edge edge2){
-        if((isAbove(edge1, edge2.n1) && !isAbove(edge1, edge2.n2))){
-            System.out.println(edge2.n1.id + " id over and " + edge2.n2.id + " is under: " + edge1.n1.id + ", " + edge1.n2.id);
-            return true;
-        } else if(!isAbove(edge1, edge2.n1) && isAbove(edge1, edge2.n2)){
-            System.out.println(edge2.n2.id + " id over and " + edge2.n1.id + " is under: " + edge1.n1.id + ", " + edge1.n2.id);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    /**
-     * This method tries to make a new edge and validate it
-     * If valid, keep the edge else delete it
-     * Return boolean per if the new edge is legal
-     * */
-    public static boolean isLegalEdge(Node node, Node newNode, ArrayList<Edge> hullEdges){
-        Edge temp = new Edge(newNode, node);
-        for(Edge edge : hullEdges){
-            if(isCrossing(temp, edge)){
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static Edge[] basicTriangulation(Node[] nodes){
-        // Create a hull and edges data structure
-        ArrayList<Edge> edges = new ArrayList<Edge>();
-        ArrayList<Node> hull = new ArrayList<Node>();
-
-        // For the nodes
-        for(Node node : nodes){
-            for(Node hullNode : hull){
-                if(isLegalEdge(hullNode, node, edges)){
-                    Edge newEdge = new Edge(hullNode, node);
-                    edges.add(newEdge);
+    public static int delaunayFlip(Point[] points, ArrayList<Edge> edges){
+        boolean isNotOptimal;
+        int flips = 0;
+        do {
+            isNotOptimal = false;
+            for(Edge e : edges){
+                // 1. Do p and q share 2 common points (a and b)
+                if(shareTwoPoints(e) == null){
+                    System.out.println("Null");
                 }
+                // 2. Does edge a-b intersect p-q
+                // 3. Do angles a and b sum over 180 degrees
             }
-            hull.add(node);
-        }
+            flips++;
+        } while(isNotOptimal);
+        return flips;
+    }
 
-        // Get the edged into an Edge array
-        Edge[] sol = new Edge[edges.size()];
-        for(int i = 0; i < sol.length; i++){
-            boolean groomed = true;
-            for(int j = 0; j < sol.length; j++){
-                Edge temp = sol[j];
-                Edge thisEdge = edges.get(i);
-                if(temp != null){
-                    if(thisEdge.n1.id == temp.n1.id && thisEdge.n2.id == temp.n2.id) {
-                        groomed = false;
-                    } else if( thisEdge.n1.id == thisEdge.n2.id){
-                        groomed = false;
+    public static void main(String[] args) {
+        try {
+            Random r;
+            n = Integer.parseInt(args[0]);
+            t = Integer.parseInt(args[1]);
+            if (args.length>2) {
+                r = new Random(Integer.parseInt(args[2]));
+            } else {
+                r = new Random();
+            }
+            points = new Point[n];
+
+            // First, create a set of unique points
+            // Our first 4 points are the outer corners.  This is not really necessary, but is
+            // intended to give us a fixed convex hull so it's easier to see if the alg is working.
+            points[0] = new Point(0.0,0.0);
+            points[1] = new Point(0.0,1.0);
+            points[2] = new Point(1.0,1.0);
+            points[3] = new Point(1.0,0.0);
+            for (int i=4;i<n;i++) {
+                boolean repeat;
+                Point np = null;
+                do {
+                    repeat = false;
+                    np = new Point(r.nextDouble(),r.nextDouble());
+                    // Verify it is a distinct point.
+                    for (int j=0;j<i;j++) {
+                        if (np.same(points[j])) {
+                            repeat = true;
+                            break;
+                        }
+                    }
+                } while(repeat);
+                points[i] = np;
+            }
+
+            System.out.println("Generated points");
+
+            // Triangulate
+            for (int i=0;i<n;i++) {
+                for (int j=i+1;j<n;j++) {
+                    Edge e = new Edge(points[i],points[j]);
+                    if (!intersection(e)) {
+                        edges.add(e);
+                        e.p.addEdge(e);
+                        e.q.addEdge(e);
                     }
                 }
             }
-            if(groomed){
-                sol[i] = edges.get(i);
-            }
+            System.out.println("Triangulated: "+n+" points, "+edges.size()+" edges");
+
+            // Now your code is required!
+            int flips = delaunayFlip(points, edges);
+            System.out.println("Flips: " + flips);
+
+        } catch (Exception e) {
+            System.out.println("ERROR " +e);
+            e.printStackTrace();
         }
-        return sol;
     }
 
-    /**
-     * Thid method takes a bunch of edges and returns one that are just on the hull
-     * */
-    public static Edge[] getHull(Edge[] edged){
-        return null;
+    public static class Point {
+        double x,y;
+
+        // Set of edges attached to this point
+        public ArrayList<Edge> edges;
+
+        public Point(double bx,double by) { x = bx; y = by; }
+
+        // Returns true if the given point is the same as this one.
+        // nb: should use machine epsilon.
+        public boolean same(Point b) { return (x == b.x && y == b.y); }
+
+        // Add an edge connection if not present; lazily creates the edge array/
+        public void addEdge(Edge e) {
+            if (edges==null) edges = new ArrayList<Edge>();
+            if (!edges.contains(e))
+                edges.add(e);
+        }
+
+        public String toString() {
+            return "("+x+","+y+")";
+        }
+    }
+
+    public static class Edge {
+        Point p,q;
+
+        public Edge(Point p1,Point p2) {
+            p=p1;
+            q=p2;
+        }
+        // Utility routine -- 2d cross-product (signed area of a triangle) test for orientation.
+        public int sat(double p0x,double p0y,double p1x,double p1y,double p2x,double p2y) {
+            double d = (p1x-p0x)*(p2y-p0y)-(p2x-p0x)*(p1y-p0y);
+            if (d<0) return -1;
+            if (d>0) return 1;
+            return 0;
+        }
+
+        // Returns true if the given edge intersects this edge.
+        public boolean intersects(Edge e) {
+            int s1 = sat(p.x,p.y, q.x,q.y, e.p.x,e.p.y);
+            int s2 = sat(p.x,p.y, q.x,q.y, e.q.x,e.q.y);
+            if (s1==s2 || (s1==0 && s2!=0) || (s2==0 && s1!=0)) return false;
+            s1 = sat(e.p.x,e.p.y, e.q.x,e.q.y, p.x,p.y);
+            s2 = sat(e.p.x,e.p.y, e.q.x,e.q.y, q.x,q.y);
+            if (s1==s2 || (s1==0 && s2!=0) || (s2==0 && s1!=0)) return false;
+            return true;
+        }
+
+        public String toString() {
+            return "<"+p+","+q+">";
+        }
     }
 }
